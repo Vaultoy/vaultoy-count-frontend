2a7527fbdfe0288fa13db778bdd4a67c2f47e8dd
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2025-08-27 13:28:00 +0200

    feat: Login and Signup form

--- src/pages/LoginSignup.tsx
+++ src/pages/LoginSignup.tsx
@@ -7,14 +7,54 @@ import {
   AbsoluteCenter,
   Button,
   Card,
-  Center,
+  Field,
   Heading,
   HStack,
   Input,
   Text,
 } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 export const LoginSignup = ({ isLogin }: { isLogin: boolean }) => {
+  const formValuesSchema = z
+    .object({
+      username: z.string().min(3).max(100),
+      password: z.string().min(8),
+      confirmPassword: isLogin ? z.string().optional() : z.string(),
+    })
+    .refine(
+      (data) => {
+        if (!isLogin) {
+          // In signup mode, confirmPassword must match password
+          return data.password === data.confirmPassword;
+        }
+        return true;
+      },
+      {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+      }
+    );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<
+    z.input<typeof formValuesSchema>,
+    any,
+    z.output<typeof formValuesSchema>
+  >({
+    resolver: zodResolver(formValuesSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  const [passwordLength, setPasswordLength] = useState(0);
+
   return (
     <AbsoluteCenter>
       <Card.Root padding="1em">
@@ -29,27 +69,51 @@ export const LoginSignup = ({ isLogin }: { isLogin: boolean }) => {
           </HStack>
         </Card.Header>
         <Card.Body>
-          <Text>Username</Text>
-          <Input />
-          <Text marginTop="1em">Password</Text>
-          <PasswordInput />
-          {!isLogin && (
-            <>
-              <Text marginTop="1em">Confirm Password</Text>
-              <PasswordInput />
-              <PasswordStrengthMeter marginTop="1em" value={3} />
-              <Text marginTop="1em" color="gray">
-                This password will be used as a key to encrypt your data.
-                <br />
-                Therefore, we recommend that you use a long and complex password
-                that you don't use anywhere else.
-              </Text>
-            </>
-          )}
+          <form onSubmit={onSubmit}>
+            <Field.Root invalid={!!errors.username}>
+              <Field.Label>Username</Field.Label>
+              <Input {...register("username")} />
+              <Field.ErrorText>{errors.username?.message}</Field.ErrorText>
+            </Field.Root>
+
+            <Field.Root invalid={!!errors.password} marginTop="1em">
+              <Field.Label>Password</Field.Label>
+              <PasswordInput
+                {...register("password")}
+                onChange={(e) => setPasswordLength(e.target.value?.length ?? 0)}
+              />
+              <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+            </Field.Root>
+            {!isLogin && (
+              <>
+                <Field.Root invalid={!!errors.confirmPassword} marginTop="1em">
+                  <Field.Label>Confirm Password</Field.Label>
+                  <PasswordInput {...register("confirmPassword")} />
+                  <Field.ErrorText>
+                    {errors.confirmPassword?.message}
+                  </Field.ErrorText>
+                </Field.Root>
+
+                <PasswordStrengthMeter
+                  marginTop="1em"
+                  value={passwordLength}
+                  max={21}
+                />
+                <Text marginTop="1em" color="gray">
+                  This password will be used as a key to encrypt your data.
+                  <br />
+                  Therefore, we recommend that you use a long and complex
+                  password that you don't use anywhere else.
+                  <br />A perfect password would be choosen randomly, 21
+                  characters long from a-z, A-Z, 0-9 and !@#$%^&*.
+                </Text>
+              </>
+            )}
+            <Button type="submit" marginTop="2em">
+              {isLogin ? "Log in" : "Sign up"}
+            </Button>
+          </form>
         </Card.Body>
-        <Card.Footer>
-          <Button>{isLogin ? "Log in" : "Sign up"}</Button>
-        </Card.Footer>
       </Card.Root>
     </AbsoluteCenter>
   );
