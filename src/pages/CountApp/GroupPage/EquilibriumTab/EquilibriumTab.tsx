34deb0322667bbbf1dad1cca5a2cf0c518bd1e2a
Thomas Sauvage <thomas-commits@sauvage.pm>
2026-03-11 13:51:30 +0100

    feat: ensure that the count is displayed as balanced even with rouding issues

--- src/pages/CountApp/GroupPage/EquilibriumTab/EquilibriumTab.tsx
+++ src/pages/CountApp/GroupPage/EquilibriumTab/EquilibriumTab.tsx
@@ -1,42 +1,68 @@
 import { Text, Card, VStack, HStack, Skeleton } from "@chakra-ui/react";
 import { CURRENCY_SYMBOL, floatCentsToString } from "@/utils/textGeneration";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { EquilibriumRepaymentsDialog } from "./EquilibriumRepaymentsDialog";
 import { GroupContext } from "@/contexts/GroupContext";
 
 export const EquilibriumTab = () => {
   const { group, isError } = useContext(GroupContext);
 
+  const numberOfNonZeroBalances = useMemo(() => {
+    if (!group) return 0;
+
+    const numberOfNonZeroBalances = group.members.reduce(
+      (count, member) => count + (member.balance !== 0 ? 1 : 0),
+      0,
+    );
+
+    if (numberOfNonZeroBalances === 1) {
+      const memberWithNonZeroBalance = group?.members.find(
+        (member) => member.balance !== 0,
+      );
+
+      console.warn(
+        `Only one member has a non-zero balance. This is expected due to rounding issues. His balance is displayed as zero.\nMember: ${memberWithNonZeroBalance?.nickname}\nBalance: ${floatCentsToString(memberWithNonZeroBalance?.balance || 0)}`,
+      );
+    }
+
+    return numberOfNonZeroBalances;
+  }, [group]);
+
   return (
     <VStack>
       <EquilibriumRepaymentsDialog />
 
-      {group?.members.map((member) => (
-        <Card.Root key={member.memberId} width="100%">
-          <Card.Body padding="0">
-            <HStack
-              justifyContent="space-between"
-              alignItems="center"
-              margin="1em"
-            >
-              <Text>{member.nickname}</Text>
+      {group?.members.map((member) => {
+        const displayedBalance =
+          numberOfNonZeroBalances !== 1 ? member.balance : 0;
 
-              <Text
-                color={
-                  member.balance > 0
-                    ? "green.600"
-                    : member.balance == 0
-                      ? "gray.400"
-                      : "red.600"
-                }
-                fontWeight="bold"
+        return (
+          <Card.Root key={member.memberId} width="100%">
+            <Card.Body padding="0">
+              <HStack
+                justifyContent="space-between"
+                alignItems="center"
+                margin="1em"
               >
-                {floatCentsToString(member.balance)}&nbsp;{CURRENCY_SYMBOL}
-              </Text>
-            </HStack>
-          </Card.Body>
-        </Card.Root>
-      ))}
+                <Text>{member.nickname}</Text>
+
+                <Text
+                  color={
+                    displayedBalance > 0
+                      ? "green.600"
+                      : displayedBalance == 0
+                        ? "gray.400"
+                        : "red.600"
+                  }
+                  fontWeight="bold"
+                >
+                  {floatCentsToString(displayedBalance)}&nbsp;{CURRENCY_SYMBOL}
+                </Text>
+              </HStack>
+            </Card.Body>
+          </Card.Root>
+        );
+      })}
 
       {!group &&
         !isError &&
